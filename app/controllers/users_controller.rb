deb31c4f96a4cf059733bed95b6b5f35178bb971
Sergio Soares <acad.sergio@gmail.com>
2016-12-15 10:17:34 -0300

    Login system implement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+  	if logged_in?
+    	@user = User.find(params[:id])
+	end    	
   end
 
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		log_in @user
+  		flash[:success] = "Start save links for study later"
   		redirect_to @user
   	else
   		render 'new'

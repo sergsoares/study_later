86b2420acde7eb38c0bcdb993abb2b91c6fde631
Sergio Soares <acad.sergio@gmail.com>
2016-12-15 09:32:55 -0300

    Prepared models and signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+	has_many :links, dependent: :destroy
+	before_save { self.email = email.downcase }
+	validates(:name, presence: true, length: {maximum: 50})
+	
+	has_secure_password
+	validates(:password, presence: true, length: {minimum: 6})
 end

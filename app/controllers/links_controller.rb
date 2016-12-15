f36d3dbbe8a6786eddc6b5c5b4cb32796af3ad6f
Sergio Soares <acad.sergio@gmail.com>
2016-12-15 10:52:52 -0300

    Simple link add and destroy implemented

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,22 @@
+class LinksController < ApplicationController
+
+  def create
+  	@user = User.find(params[:user_id])
+	@link = @user.links.create(link_params)
+	# redirect_to link_path(@link)
+	redirect_to @user
+  end
+
+def destroy
+	@user = User.find(params[:user_id])
+	@link = @user.links.find(params[:id])
+	@link.destroy
+	redirect_to @user
+end
+
+  private
+	def link_params
+		params.require(:link).permit(:name, :link)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    resources :links
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
